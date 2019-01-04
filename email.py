3f9e69c1cc4cc719dea879a50f10c10786bc2595
ytlamal <ytlamal@connect.ust.hk>
2019-01-04 03:09:19 -0800

    Add files via upload

---
+++ email.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+import time
+
+url = 'https://protonmail.com/'
+
+driver = webdriver.Chrome('/home/ytlamal/Downloads/chromedriver')
+driver.get(url)
+time.sleep(2)
+
+driver.find_element_by_xpath('//*[@title="Sign Up"]').click()
+
+time.sleep(2)
+
+driver.find_element_by_class_name('panel-heading').click()
+
+time.sleep(4)
+
+driver.find_element_by_id('freePlan').click()
+
+time.sleep(2)
+
+driver.find_element_by_id('username').send_keys('usernameForUser')
+
+time.sleep(1.5)
+
+driver.find_element_by_id('password').send_keys('passwordForUser')
+
+time.sleep(2)
+
+driver.find_element_by_id('passwordc').send_keys('passwordForUser')
+
+time.sleep(2)
+
+driver.find_element_by_class_name('signUpProcess-btn-create').click()
+
+time.sleep(1)
+
+driver.find_element_by_id('confirmModalBtn').click()
+
+
+
